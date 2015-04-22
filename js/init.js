8b474578fc426c58cf8cf88e98505de7bf4b14dc
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-04-22 12:27:46 -0500

    Historic map and modern map added

--- js/init.js
+++ js/init.js
@@ -3,8 +3,24 @@ $( document ).ready(function() {
     init();
 });
 
+/* Globals */
+var ServerVars,
+	AppVars = {
+		map: {},
+		mapBounds: {
+			north: 27.5,
+			south: 15,
+			east: 99.5,
+			west: 90.5
+		},
+		maxZoom: 10,
+		minZoom: 4
+	},
+	DataVars;
+
 function init() {
 	initEvents();
+	initMap();
 }
 
 function initEvents(){
---
+++ js/map.js
@@ -0,0 +1,21 @@
+function initMap(){
+	AppVars.map = L.map( "map", { 
+		zoomControl: false, 
+		attributionControl: false,
+		maxZoom: AppVars.maxZoom,
+		minZoom: AppVars.minZoom
+	} ).setView( [19,95], 6 );
+	
+	/*placeholder tiles for now */
+	modernTiles = L.tileLayer( "http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png" , { maxNativeZoom : 18, maxZoom : 21 }).addTo(AppVars.map);
+	
+	var bounds = L.latLngBounds([ 
+		[AppVars.mapBounds.south, AppVars.mapBounds.west],
+		[AppVars.mapBounds.north, AppVars.mapBounds.east]
+	]);
+	
+	historicTiles = L.tileLayer( "tiles/{z}/{x}/{y}.png", {
+		tms : true,
+		bounds: bounds
+	}).addTo(AppVars.map);
+}
