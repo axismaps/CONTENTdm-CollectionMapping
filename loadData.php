59e624e4d927b6e2a14007d41ac5dbffd4fddd2a
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-04-22 14:29:37 -0500

    PHP loads all data into cache file

--- loadData.php
+++ loadData.php
@@ -13,6 +13,21 @@ function checkCacheAge() {
 }
 
 function loadData( ){
+	$ch = curl_init();
+	$cache = fopen( "data.json", "w" );
+	
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false); //TODO: Trust actual certificate instead of all certificates. Do we need to do this or not?
+	curl_setopt( $ch, CURLOPT_FILE, $cache );
+	curl_setopt( $ch, CURLOPT_URL, "https://server15963.contentdm.oclc.org/dmwebservices/index.php?q=dmQuery/p15963coll18/0/title!subjec!date!covera/title/1024/1/0/0/0/0/0/0/json" );
+	
+	if( curl_exec( $ch ) === false ) {
+		echo 'Curl error: ' . curl_error( $ch );
+	}
+	
+	curl_close( $ch );
+	fclose( $cache );
+	
+	
 	$data = '';
 	
 	//process data into json
