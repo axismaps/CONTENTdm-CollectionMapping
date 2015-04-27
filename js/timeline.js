8c67efbab82c2a8e053cd0746a06ba9893a54711
Andy Woodruff <awoodruff@gmail.com>
2015-04-27 14:08:33 -0400

    Basic listing of entries within year

--- js/timeline.js
+++ js/timeline.js
@@ -10,6 +10,15 @@ function drawTimeline(){
 		drawYear( AppVars.years[i] );
 	}
 	selectYear( AppVars.years[0] );
+
+	$( ".entry-title" ).click( function(){
+		var $entry = $(this).parent(),
+			$year = $entry.parent();
+		if ( !$year.hasClass( "active" ) || $entry.hasClass( "expanded" ) ) return;
+		var totalHeight = $entry.parent().height();
+		$( ".timeline-entry.expanded", $entry.parent() ).removeClass( "expanded" );
+		$entry.addClass( "expanded" );
+	});
 }
 
 function drawYear( year ){
@@ -17,6 +26,18 @@ function drawYear( year ){
 		.attr( "class", "timeline-year" )
 		.attr( "id", "year" + year )
 		.appendTo( "#timeline-inner" );
+
+	var entries = _.filter( DataVars.data.entries, function(d){
+		return d.date.year == year;
+	});
+	_.each( entries, function(d,i){
+		var $entry = $( "<div class='timeline-entry e" + i + "'>" );
+		$entry.append( "<p class='entry-title'>" + d.title + "</p>" )
+			.append( "<p>Subject: " + d.subjec + "<p>" );
+		if ( i == 0 ) $entry.addClass( "expanded" );
+
+		$div.append( $entry );
+	});
 }
 
 function selectYear( year ){
