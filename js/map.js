819f009f49706202bcdb38e5385e4a6475343d75
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-04-28 12:57:32 -0500

    Show results on map

--- js/map.js
+++ js/map.js
@@ -10,14 +10,14 @@ function initMap(){
 	/* TODO: Do we want these? If not, restrict bounds so you can't see anything beyond historicTiles */
 	modernTiles = L.tileLayer( "http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png" , { maxNativeZoom : 18, maxZoom : 21 }).addTo(AppVars.map);
 	
-	var bounds = L.latLngBounds([ 
+	AppVars.mapBounds.bounds = L.latLngBounds([ 
 		[AppVars.mapBounds.south, AppVars.mapBounds.west],
 		[AppVars.mapBounds.north, AppVars.mapBounds.east]
 	]);
 	
 	historicTiles = L.tileLayer( "tiles/{z}/{x}/{y}.png", {
 		tms : true,
-		bounds: bounds
+		bounds: AppVars.mapBounds.bounds
 	}).addTo(AppVars.map);
 	
 	drawPoints();
@@ -38,7 +38,38 @@ function drawPoints(){
 		
 		function geocode( location ){
 			console.log( location );
-			// MQ.geocode().search( location )
+			MQ.geocode().search( location )
+				.on( 'success', function( e ){
+					
+					if( e.result.matches.length === 0 ){
+						console.log( 'Couldn\'t find one' );
+					} else {
+						var result;
+						
+						$.map( e.result.matches.reverse(), function( v, i ){
+							if( AppVars.mapBounds.bounds.contains( v.latlng ) ) {
+								result = v;
+								return;
+							}
+						});
+						
+						if( result ) {
+							var latlng = result.latlng;
+
+							// var geocodeIcon = L.icon({
+								// iconUrl: 'img/' + selectedCategory + '-marker.png',
+								// iconSize: [62, 82],
+								// iconAnchor: [31, 82]
+							// });
+							
+							L.marker( [ latlng.lat, latlng.lng ] )
+								.addTo( AppVars.map );
+						
+						} else {
+							console.log( 'Location is off the map' );
+						}
+					}
+				});
 		}
 	});
 }
