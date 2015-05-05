e5e4ae489564db01b4cdd3fa3309d6566df03dfb
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-05-05 17:11:35 -0500

    Attempt with custom data source

--- js/search.js
+++ js/search.js
@@ -1,19 +1,30 @@
 function initSearch(){
-	var entries = new Bloodhound({
-		datumTokenizer: Bloodhound.tokenizers.whitespace,
-		queryTokenizer: Bloodhound.tokenizers.whitespace,
-		local: DataVars.data
-	});
+	// var entries = new Bloodhound({
+		// datumTokenizer: Bloodhound.tokenizers.whitespace,
+		// queryTokenizer: Bloodhound.tokenizers.whitespace,
+		// local: DataVars.data
+	// });
 	
 	$( '#search .typeahead' ).typeahead({
 		highlight: true,
 		minLength: 2
 	},{
 		name: 'searchResults',
-		source: entries
+		source: search()
 	});
 }
 
-function search(){
-	
-}
+var search = function(){
+	return function findMatches( q, cb ){
+		var matches=[],
+			substringRegex = new RegExp(q, 'i' );
+			
+		_.each( DataVars.data.entries, function( v, k, l ){
+			if( substringRegex.test( v.descri ) || substringRegex.test( v.title ) ){
+				matches.push( v.title );
+			}
+		});
+		
+		cb( matches );
+	};
+};
