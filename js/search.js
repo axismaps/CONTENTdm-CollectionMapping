1d608a79f8e0b4478841781f0b659f9e7f3914c7
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-05-06 09:54:16 -0500

    Searching affects map and timeline

--- js/search.js
+++ js/search.js
@@ -6,6 +6,10 @@ function initSearch(){
 		name: 'searchResults',
 		source: search()
 	});
+	
+	$( '.typeahead' ).on( 'typeahead:selected', function( e, suggestion ){
+		displayResults( suggestion );
+	});
 }
 
 var search = function(){
@@ -15,10 +19,16 @@ var search = function(){
 			
 		_.each( DataVars.data.entries, function( v, k, l ){
 			if( substringRegex.test( v.descri ) || substringRegex.test( v.title ) ){
-				matches.push( {'value': v.title });
+				matches.push( {'value': v.title, 'pointer': v.pointer, 'year': v.date.year });
 			}
 		});
 		
 		cb( matches );
 	};
 };
+
+function displayResults( suggestion ){
+	AppVars.selectedPoint = suggestion.pointer;
+	selectYear( suggestion.year );
+	$( '#entry' + suggestion.pointer + ' .entry-title' ).click();
+}
