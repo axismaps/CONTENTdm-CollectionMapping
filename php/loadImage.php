4797ced7a15d7e81b3cba698d13417c90785b0f1
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-05-07 12:26:38 -0500

    Individual temp files per image
    In case of collisions.

--- php/loadImage.php
+++ php/loadImage.php
@@ -18,14 +18,19 @@ if( ! file_exists( 'images\\' . $id . '-' . $size . '.jpg' ) ){
 	
 	//file is a pdf
 	if( substr( $file_info->{'find'}, -3 ) == 'pdf' ) {
-	
-		file_put_contents( 'temp.pdf', file_get_contents( 'http://cdm15963.contentdm.oclc.org/utils/getfile/collection/p15963coll18/id/' . $id ) );
+		
+		$i = 0;
+		while( file_exists( 'temp' . $i . '.pdf' )) {
+			$i++;
+		}
+		
+		file_put_contents( 'temp' . $i . '.pdf', file_get_contents( 'http://cdm15963.contentdm.oclc.org/utils/getfile/collection/p15963coll18/id/' . $id ) );
 
 		//convert pdf page to jpg
 		$imagick = new Imagick();
 		$imagick->setResolution(300,300);
 		
-		$imagick->readimage( getcwd() . '\temp.pdf[0]' );
+		$imagick->readimage( getcwd() . '\temp' . $i . '.pdf[0]' );
 		$imagick->setImageFormat( 'jpeg' );
 		$imagick->writeImage( getcwd() . '\images\\' . $id . '-full.jpg' );
 			
@@ -35,12 +40,18 @@ if( ! file_exists( 'images\\' . $id . '-' . $size . '.jpg' ) ){
 		$imagick->clear();
 		$imagick->destroy();
 		
-		unlink( "temp.pdf" );
+		unlink( "temp" . $i . ".pdf" );
 	} elseif ( substr( $file_info->{'find'}, -3 ) == 'jpg' OR substr( $file_info->{'find'}, -4 ) == 'jpeg' ){
-		file_put_contents( 'temp.jpg', file_get_contents( 'http://cdm15963.contentdm.oclc.org/utils/getfile/collection/p15963coll18/id/' . $id ) );
+		
+		$i = 0;
+		while( file_exists( 'temp' . $i . '.jpg' )) {
+			$i++;
+		}
+		
+		file_put_contents( 'temp' . $i . '.jpg', file_get_contents( 'http://cdm15963.contentdm.oclc.org/utils/getfile/collection/p15963coll18/id/' . $id ) );
 		
 		$imagick = new Imagick();
-		$imagick->readimage( getcwd() . '\temp.jpg' );
+		$imagick->readimage( getcwd() . '\temp' . $i . '.jpg' );
 		$imagick->setImageFormat( 'jpeg' );
 		$imagick->writeImage( getcwd() . '\images\\' . $id . '-full.jpg' );
 		
@@ -49,10 +60,10 @@ if( ! file_exists( 'images\\' . $id . '-' . $size . '.jpg' ) ){
 		
 		$imagick->clear();
 		$imagick->destroy();
-		unlink( "temp.jpg" );
+		unlink( "temp" . $i . ".jpg" );
 	}
 }
 
-header( "Location: images/$id-$size.jpg" );
-
+header( "Location: images/$id-$size.jpg", TRUE, 302 );
+exit;
 ?>
